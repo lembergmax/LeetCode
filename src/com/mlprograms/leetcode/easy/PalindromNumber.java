779e99b4af046e3c43e82ba83afb9a4d3cb2aed3
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-01 14:30:35 +0200

    add PalindromNumber class with palindrome checking functionality

---
+++ src/com/mlprograms/leetcode/easy/PalindromNumber.java
@@ -0,0 +1,28 @@
+package com.mlprograms.leetcode.easy;
+
+public class PalindromNumber {
+
+    public static boolean isPalindrome(int x) {
+        String stringX = String.valueOf(x);
+
+        int pointerLeft = 0;
+        int pointerRight = stringX.length() - 1;
+        while(pointerLeft <= pointerRight) {
+            if(stringX.charAt(pointerLeft) != stringX.charAt(pointerRight)) {
+                return false;
+            }
+
+            pointerLeft++;
+            pointerRight--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));  // Output: true
+        System.out.println(isPalindrome(-121)); // Output: false
+        System.out.println(isPalindrome(10));   // Output: false
+    }
+
+}
