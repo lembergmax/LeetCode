a7eb233bd36bb1f621344fe57c3d4e50e416a2ee
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-02 12:09:01 +0200

    add ValidPalindrom class with method to validate palindrome strings

---
+++ src/com/mlprograms/leetcode/easy/ValidPalindrom.java
@@ -0,0 +1,35 @@
+package com.mlprograms.leetcode.easy;
+
+public class ValidPalindrom {
+
+    public static boolean isPalindrome(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char character : s.toCharArray()) {
+            if (Character.isAlphabetic(character) || Character.isDigit(character)) {
+                stringBuilder.append(String.valueOf(character).toLowerCase());
+            }
+        }
+        String sbString = stringBuilder.toString();
+
+        int pointerLeft = 0;
+        int pointerRight = sbString.length() - 1;
+        while (pointerLeft <= pointerRight) {
+            if (sbString.charAt(pointerLeft) != sbString.charAt(pointerRight)) {
+                return false;
+            }
+
+            pointerLeft++;
+            pointerRight--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome(" "));
+        System.out.println(isPalindrome("0P"));
+    }
+
+}
