15b297676705db952195bc6cb55c602721494049
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-02 11:46:54 +0200

    add ValidParentheses class with method to validate parentheses strings

---
+++ src/com/mlprograms/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,33 @@
+package com.mlprograms.leetcode.easy;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char character : s.toCharArray()) {
+            if (character == '(') {
+                stack.push(')');
+            } else if (character == '[') {
+                stack.push(']');
+            } else if (character == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || stack.pop() != character) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));      // true
+        System.out.println(isValid("()[]{}"));  // true
+        System.out.println(isValid("(]"));      // false
+        System.out.println(isValid("([])"));    // true
+        System.out.println(isValid("([)]"));    // false
+    }
+
+}
