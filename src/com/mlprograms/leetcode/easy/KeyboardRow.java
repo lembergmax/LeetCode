d248cd178a4e1053d1397e82be100eef8483731e
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-03 13:23:33 +0200

    add KeyboardRow class with method to find words that can be typed using a single row of the keyboard

---
+++ src/com/mlprograms/leetcode/easy/KeyboardRow.java
@@ -0,0 +1,43 @@
+package com.mlprograms.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KeyboardRow {
+
+    private static final String row1 = "qwertyuiop";
+    private static final String row2 = "asdfghjkl";
+    private static final String row3 = "zxcvbnm";
+
+    public static String[] findWords(String[] words) {
+        List<String> rows = Arrays.asList(row1, row2, row3);
+        List<String> wordsWithinRow = new ArrayList<>(List.of());
+        String currentWordInLowercase;
+
+        for (String row : rows) {
+            for (String word : words) {
+                currentWordInLowercase = word.toLowerCase();
+                for (char character : word.toCharArray()) {
+                    String characterAsStringInLowercase = String.valueOf(character).toLowerCase();
+                    if (row.contains(characterAsStringInLowercase)) {
+                        currentWordInLowercase = currentWordInLowercase.replace(characterAsStringInLowercase, "");
+                    }
+                }
+                if (currentWordInLowercase.isBlank() && !wordsWithinRow.contains(currentWordInLowercase)) {
+                    wordsWithinRow.add(word);
+                }
+            }
+        }
+
+        return wordsWithinRow.toArray(new String[0]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(findWords(new String[]{"Hello", "Alaska", "Dad", "Peace"})));
+        // [" Alaska"," Papa "]
+        System.out.println(Arrays.toString(findWords(new String[]{"omk"})));
+        System.out.println(Arrays.toString(findWords(new String[]{"adsdf", "sfd"})));
+    }
+
+}
