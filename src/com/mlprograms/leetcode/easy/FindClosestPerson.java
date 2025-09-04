72727d8cc167769685b3069606c1fdd1ea5bb911
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-04 08:05:32 +0200

    add FindClosestPerson class with method to determine closest person based on distance

---
+++ src/com/mlprograms/leetcode/easy/FindClosestPerson.java
@@ -0,0 +1,17 @@
+package com.mlprograms.leetcode.easy;
+
+public class FindClosestPerson {
+
+    public static int findClosest(int x, int y, int z) {
+        int distanceXtoZ = Math.abs(z - x);
+        int distanceYtoZ = Math.abs(z - y);
+        return distanceXtoZ > distanceYtoZ ? 2 : distanceXtoZ == distanceYtoZ ? 0 : 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findClosest(2, 7, 4));   // 1
+        System.out.println(findClosest(2, 5, 6));   // 2
+        System.out.println(findClosest(1, 5, 3));   // 0
+    }
+
+}
