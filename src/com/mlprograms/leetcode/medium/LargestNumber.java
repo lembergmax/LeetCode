cbb56504583c307411b902bd1dd5960a127be5e3
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-05 17:30:14 +0200

    add LargestNumber class with method to form the largest number from an array of integers

---
+++ src/com/mlprograms/leetcode/medium/LargestNumber.java
@@ -0,0 +1,32 @@
+package com.mlprograms.leetcode.medium;
+
+import java.util.Arrays;
+
+public class LargestNumber {
+
+    public static String largestNumber(int[] nums) {
+        String[] stringNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            stringNums[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(stringNums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (stringNums[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : stringNums) {
+            stringBuilder.append(string);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestNumber(new int[]{10, 2}));
+        System.out.println(largestNumber(new int[]{3, 30, 34, 5, 9}));
+    }
+
+}
