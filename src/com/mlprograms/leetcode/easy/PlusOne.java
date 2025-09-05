2daabec93452dfc0c911c5502c1b58e4e215c56f
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-05 16:21:34 +0200

    add PlusOne class with method to increment an array of digits by one

---
+++ src/com/mlprograms/leetcode/easy/PlusOne.java
@@ -0,0 +1,33 @@
+package com.mlprograms.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+        List<Integer> newDigits = new ArrayList<>();
+        int carry = 1;
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int sum = digits[i] + carry;
+            newDigits.addFirst(sum % 10);
+            carry = sum / 10;
+        }
+
+        if (carry > 0) {
+            newDigits.addFirst(carry);
+        }
+
+        return newDigits.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(plusOne(new int[]{4, 3, 2, 1})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9})));
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0})));
+    }
+
+}
