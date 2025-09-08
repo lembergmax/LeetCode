b2714e49d234592bc9516d98ecc2f501d6c0d4ca
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-08 14:34:14 +0200

    update main method output in MedianOfTwoSortedArrays class for correct median value

--- src/com/mlprograms/leetcode/hard/MedianOfTwoSortedArrays.java
+++ src/com/mlprograms/leetcode/hard/MedianOfTwoSortedArrays.java
@@ -23,7 +23,7 @@ public class MedianOfTwoSortedArrays {
 
     public static void main(String[] args) {
         System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2, 4, 5}));    // 3.0
-        System.out.println(findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));       // 2.0
+        System.out.println(findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));       // 2.5
     }
 
 }
