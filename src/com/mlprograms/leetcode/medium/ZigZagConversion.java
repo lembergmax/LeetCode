728439d3944e2c7ac9c38c9471e3aada5c7db3e9
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-08 08:22:55 +0200

    add ZigZagConversion class with method to convert string to zigzag pattern

---
+++ src/com/mlprograms/leetcode/medium/ZigZagConversion.java
@@ -0,0 +1,53 @@
+package com.mlprograms.leetcode.medium;
+
+import java.util.Arrays;
+
+public class ZigZagConversion {
+
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        char[][] zigzag2DArray = new char[s.length()][numRows];
+
+        int x = 0;
+        int y = 0;
+        boolean add = true;
+        for (char character : s.toCharArray()) {
+            zigzag2DArray[x][y] = character;
+
+            if (add) {
+                y++;
+            } else {
+                y--;
+                x++;
+            }
+
+            if (y >= numRows - 1) {
+                add = false;
+            } else if (y <= 0) {
+                add = true;
+            }
+        }
+
+        StringBuilder zigzagString = new StringBuilder();
+        for (int dy = 0; dy < numRows; dy++) {
+            for (int dx = 0; dx < s.length(); dx++) {
+                char character = zigzag2DArray[dx][dy];
+                if (character != 0) {
+                    zigzagString.append(character);
+                }
+            }
+        }
+
+        return zigzagString.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("PAYPALISHIRING", 3));
+        System.out.println(convert("PAYPALISHIRING", 4));
+        System.out.println(convert("A", 1));
+    }
+
+}
