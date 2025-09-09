90f034cf6c5b793d233dd31e507e948bde3fcbc7
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-09 14:42:37 +0200

    implement regex matching logic in isMatch method of RegularExpressionMatching class

--- src/com/mlprograms/leetcode/hard/RegularExpressionMatching.java
+++ src/com/mlprograms/leetcode/hard/RegularExpressionMatching.java
@@ -3,19 +3,42 @@ package com.mlprograms.leetcode.hard;
 public class RegularExpressionMatching {
 
     public static boolean isMatch(String s, String p) {
-        int indexS = 0;
-        int indexP = 0;
-        char currentCharacterS = p.charAt(indexS);
-        char currentCharacterP = p.charAt(indexP);
-        // Iteriere über jeden Buchstaben in s
+        int textLength = s.length();
+        int patternLength = p.length();
 
-        return false;
+        boolean[][] matches = new boolean[textLength + 1][patternLength + 1];
+        matches[0][0] = true;
+
+        for (int j = 2; j <= patternLength; j++) {
+            if (p.charAt(j - 1) == '*') {
+                matches[0][j] = matches[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= textLength; i++) {
+            for (int j = 1; j <= patternLength; j++) {
+                char currentPatternChar = p.charAt(j - 1);
+
+                if (currentPatternChar == '.' || currentPatternChar == s.charAt(i - 1)) {
+                    matches[i][j] = matches[i - 1][j - 1];
+                } else if (currentPatternChar == '*') {
+                    matches[i][j] = matches[i][j - 2];
+
+                    char precedingPatternChar = p.charAt(j - 2);
+                    if (precedingPatternChar == '.' || precedingPatternChar == s.charAt(i - 1)) {
+                        matches[i][j] = matches[i][j] || matches[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return matches[textLength][patternLength];
     }
 
     public static void main(String[] args) {
-        System.out.println(isMatch("aa", "a"));
-        System.out.println(isMatch("aa", "a*"));
-        System.out.println(isMatch("ab", ".*"));
+        System.out.println(isMatch("aa", "a"));     // false
+        System.out.println(isMatch("aa", "a*"));    // true
+        System.out.println(isMatch("ab", ".*"));    // true
     }
 
 }
