3e21e51d59679bb8a41b4ac617ef895ee658c1bb
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-09 09:52:23 +0200

    add RegularExpressionMatching class with method to evaluate regex matching

---
+++ src/com/mlprograms/leetcode/hard/RegularExpressionMatching.java
@@ -0,0 +1,15 @@
+package com.mlprograms.leetcode.hard;
+
+public class RegularExpressionMatching {
+
+    public static boolean isMatch(String s, String p) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aa", "a"));
+        System.out.println(isMatch("aa", "a*"));
+        System.out.println(isMatch("ab", ".*"));
+    }
+
+}
