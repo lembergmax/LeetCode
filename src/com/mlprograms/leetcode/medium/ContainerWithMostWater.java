bc411285c3f475d35bf5f0b27a135f4df3a77d6f
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-10 13:08:42 +0200

    implement maxArea method to calculate maximum water container area

--- src/com/mlprograms/leetcode/medium/ContainerWithMostWater.java
+++ src/com/mlprograms/leetcode/medium/ContainerWithMostWater.java
@@ -3,12 +3,22 @@ package com.mlprograms.leetcode.medium;
 public class ContainerWithMostWater {
 
     public static int maxArea(int[] height) {
-        return 0;
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = 0; j < height.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * Math.abs(i - j));
+            }
+        }
+
+        return maxArea;
     }
 
     public static void main(String[] args) {
-        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
-        System.out.println(maxArea(new int[]{1, 1}));
+        maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
     }
 
 }
