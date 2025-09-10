ed599745b0f0b5ed07dc24271086dd14b80b449d
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-10 21:03:10 +0200

    add: implement longestCommonPrefixImproved method for enhanced prefix matching

--- src/com/mlprograms/leetcode/easy/LongestCommonPrefix.java
+++ src/com/mlprograms/leetcode/easy/LongestCommonPrefix.java
@@ -1,6 +1,7 @@
 package com.mlprograms.leetcode.easy;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LongestCommonPrefix {
 
@@ -27,6 +28,32 @@ public class LongestCommonPrefix {
         }
 
         return prefixList.isEmpty() ? "" : prefixList.getLast();
+    }
+
+    public static String longestCommonPrefixImproved(String[] strs) {
+        String firstWord = strs[strs.length - 1];
+        int length = firstWord.length();
+        List<String> prefixList = new ArrayList<>(length);
+
+        for (int i = 0; i < length; i++) {
+            prefixList.add(firstWord.substring(0, i));
+        }
+
+        for (int i = 1; i < strs.length; i++) {
+            if (prefixList.isEmpty()) {
+                break;
+            }
+
+            while (!strs[i].startsWith(prefixList.get(0))) {
+                prefixList.removeLast();
+
+                if (prefixList.isEmpty()) {
+                    break;
+                }
+            }
+        }
+
+        return prefixList.isEmpty() ? "" : prefixList.getFirst();
     }
 
     public static void main(String[] args) {
