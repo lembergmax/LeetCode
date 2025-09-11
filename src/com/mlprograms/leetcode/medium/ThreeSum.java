f9dd8a6e7ac7be38edf200a79fc11159a25f524e
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-11 13:35:54 +0200

    add: implement threeSum method to find unique triplets that sum to zero

--- src/com/mlprograms/leetcode/medium/ThreeSum.java
+++ src/com/mlprograms/leetcode/medium/ThreeSum.java
@@ -1,24 +1,50 @@
 package com.mlprograms.leetcode.medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
 
     public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-        // 1. Sort the given array in non-decreasing order.
-        nums = Arrays.stream(nums).sorted().toArray();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-        // 2. Loop through the array from index 0 to n-1.
-        // 3. For each iteration, set the target as -nums[i].
-        // 4. Set two pointers, j=i+1 and k=n-1.
-        // 5. While j<k, check if nums[j]+nums[k]==target.
-        // 6. If yes, add the triplet {nums[i], nums[j], nums[k]} to the result and move j to the right and k to the left.
-        // 7. If no, move either j or k based on the comparison of nums[j]+nums[k] with target.
-        // 8. To avoid duplicate triplets, skip the iterations where nums[i]==nums[i-1] and also skip the iterations where nums[j]==nums[j-1] or nums[k]==nums[k+1].
+            int pointerJ = i + 1;
+            int pointerK = nums.length - 1;
 
-        return List.of(List.of());
+            while (pointerJ < pointerK) {
+                int sum = nums[i] + nums[pointerJ] + nums[pointerK];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[pointerJ], nums[pointerK]));
+
+                    while (pointerJ < pointerK && nums[pointerJ] == nums[pointerJ + 1]) {
+                        pointerJ++;
+                    }
+                    while (pointerJ < pointerK && nums[pointerK] == nums[pointerK - 1]) {
+                        pointerK--;
+                    }
+
+                    pointerJ++;
+                    pointerK--;
+                    continue;
+                }
+
+                if (sum < 0) {
+                    pointerJ++;
+                } else {
+                    pointerK--;
+                }
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
