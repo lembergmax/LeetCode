f3a95fd8fa8de60921865922d673856f9abce3d3
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-12 09:47:05 +0200

    add: implement ThreeSumClosest class with method to find closest sum to target

---
+++ src/com/mlprograms/leetcode/medium/ThreeSumClosest.java
@@ -0,0 +1,14 @@
+package com.mlprograms.leetcode.medium;
+
+public class ThreeSumClosest {
+
+    public static int threeSumClosest(int[] nums, int target) {
+        return 0; // TODO: implement
+    }
+
+    public static void main(String[] args) {
+        System.out.println(threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+        System.out.println(threeSumClosest(new int[]{0, 0, 0}, 1));
+    }
+
+}
