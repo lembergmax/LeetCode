3e1b82955f9f11bc161f4b82e5608ac8ecf054a7
Max Lemberg <max.lemberg2402@gmail.com>
2025-09-15 20:02:54 +0200

    add: new class GenerateParentheses to generate all valid parentheses

---
+++ src/com/mlprograms/leetcode/medium/GenerateParentheses.java
@@ -0,0 +1,34 @@
+package com.mlprograms.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generate(result, "", 0, 0, n);
+        return result;
+    }
+
+    private static void generate(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+
+        if (open < max) {
+            generate(result, current + "(", open + 1, close, max);
+        }
+
+        if (close < open) {
+            generate(result, current + ")", open, close + 1, max);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(3));
+        System.out.println(generateParenthesis(1));
+    }
+
+}
