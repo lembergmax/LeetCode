707fbc8757d0d8de6e46875dbad031a410dc8997
max.lemberg <max.lemberg2402@gmail.com>
2025-09-22 12:49:44 +0200

    add: implement method to remove duplicates from sorted array

---
+++ src/com/mlprograms/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+package com.mlprograms.leetcode.easy;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static int removeDuplicates(int[] nums) {
+        return 0; // TODO
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1, 1, 2}));                       // 2
+        System.out.println(removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));  // 5
+    }
+
+}
