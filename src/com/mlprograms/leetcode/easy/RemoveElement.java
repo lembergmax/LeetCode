002b938bfb7f6f5e98177f249eed367a86f98716
max.lemberg <max.lemberg2402@gmail.com>
2025-09-23 07:39:52 +0200

    add: implement method to remove elements from an array

---
+++ src/com/mlprograms/leetcode/easy/RemoveElement.java
@@ -0,0 +1,19 @@
+package com.mlprograms.leetcode.easy;
+
+public class RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+        return 0; // TODO
+    }
+
+    public static void main(String[] args) {
+        int[] nums;
+
+        nums = new int[]{3, 2, 2, 3};
+        System.out.println(removeElement(nums, 3));
+
+        nums = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(removeElement(nums, 2));
+    }
+
+}
