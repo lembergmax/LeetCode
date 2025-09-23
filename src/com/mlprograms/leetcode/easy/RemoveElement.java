728e6938a130857f26a150f3c1c45662fb194179
max.lemberg <max.lemberg2402@gmail.com>
2025-09-23 13:01:29 +0200

    add: implement method to remove specified value from an array

--- src/com/mlprograms/leetcode/easy/RemoveElement.java
+++ src/com/mlprograms/leetcode/easy/RemoveElement.java
@@ -4,17 +4,26 @@ package com.mlprograms.leetcode.easy;
 public class RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
-        return 0; // TODO
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
     }
 
     public static void main(String[] args) {
         int[] nums;
 
         nums = new int[]{3, 2, 2, 3};
-        System.out.println(removeElement(nums, 3));
+        System.out.println(removeElement(nums, 3)); // 2
 
         nums = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
-        System.out.println(removeElement(nums, 2));
+        System.out.println(removeElement(nums, 2)); // 5
     }
 
 }
